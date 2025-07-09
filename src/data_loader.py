c22d19a190cf7c64e7a151ef978ee5ff9fa2a525
Mohammed Like <mohammedlike.01@gmail.com>
2025-07-09 21:56:38 +0530

    first commit

---
+++ src/data_loader.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+import pandas as pd
+
+def get_short_rate_data(ticker='^IRX', start='2015-01-01', end='2024-12-31'):
+    data = yf.download(ticker, start=start, end=end)
+    # Use 'Close' if 'Adj Close' is not present
+    if 'Adj Close' in data.columns:
+        data = data['Adj Close'] / 100
+    elif 'Close' in data.columns:
+        data = data['Close'] / 100
+    else:
+        raise KeyError("Neither 'Adj Close' nor 'Close' found in data columns: " + str(data.columns))
+    data = data.dropna()
+    data.name = "ShortRate"
+    return data
+
